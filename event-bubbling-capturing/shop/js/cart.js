ae998e08b0f6c4d3427502f0963ddb8be16660ca
Grinch <anger@211.ru>
2018-10-17 23:34:54 +0700

    ok

--- event-bubbling-capturing/shop/js/cart.js
+++ event-bubbling-capturing/shop/js/cart.js
@@ -1,1 +1,20 @@
 'use strict';
+
+handler();
+
+showMore.addEventListener( 'click', handler);
+
+function handler(){
+	const addToCartButton = document.querySelectorAll( '.add-to-cart' );
+	Array
+	.from(addToCartButton)
+	.forEach((el) => {
+		el.addEventListener( 'click', (event) => {
+			event.preventDefault();
+			addToCart( event.target.dataset )
+		})		
+	})	
+}
+
+
+
