99cbab66d6b5120e0b0b58b9e7d25baffbc66164
Grinch <anger@211.ru>
2018-10-22 23:47:53 +0700

    ok

--- local-storage/counter/js/counter.js
+++ local-storage/counter/js/counter.js
@@ -1,1 +1,25 @@
 'use strict';
+const counter = document.querySelector('#counter');
+
+if ( localStorage.counter ){
+  counter.textContent = localStorage.counter;
+} else { 
+  counter.textContent = '0';
+};
+
+document.querySelector('.wrap-btns')
+  .addEventListener('click', e => {
+  switch( event.target.id ){
+    case "increment":
+      counter.textContent = +counter.textContent + 1;
+      break;
+    case 'decrement':
+      if( counter.textContent > 0 )
+      counter.textContent = +counter.textContent - 1;
+      break;
+    case 'reset':
+      counter.textContent = 0;   
+  };
+  localStorage.counter = counter.textContent;
+});
+
