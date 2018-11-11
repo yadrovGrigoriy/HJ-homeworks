2cae3676d9a7f8a2ff8840d50fb59382b781f7de
Grinch <anger@211.ru>
2018-11-11 20:56:38 +0700

    ok

--- websocket/bubbles/js/click.js
+++ websocket/bubbles/js/click.js
@@ -1,1 +1,12 @@
 'use strict';
+const connection = new WebSocket('wss://neto-api.herokuapp.com/mouse');
+showBubbles(connection)
+
+document.addEventListener('click', () => {
+   connection.send(
+     JSON.stringify({
+      x: event.x,
+      y: event.y  
+     })
+   );
+}) 
