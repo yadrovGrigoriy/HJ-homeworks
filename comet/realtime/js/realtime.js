a923b2490f8d25b7a92af94584878d4c780558ae
Grinch <anger@211.ru>
2018-11-12 00:09:54 +0700

    ok

--- comet/realtime/js/realtime.js
+++ comet/realtime/js/realtime.js
@@ -13,17 +13,16 @@ const realtime = new Chart(ctx).Bar({
 
 let isFirst = true;
 const ws = new WebSocket('wss://neto-api.herokuapp.com/realtime');
-ws.addEventListener('message', event => {
-  if (isFirst) {
-    event.data
-      .split('\n')
-      .map(line => line.split('|'))
-      .forEach(data => realtime.addData([Number(data[1])], data[0]));
 
+ws.addEventListener('message', event => {
+  const item = JSON.parse(event.data);
+  if (isFirst) {
+    item.reverse().forEach(data => realtime.addData([data.online], data.time));
+    
     isFirst = false;
   } else {
-    const [label, data] = event.data.split('|');
     realtime.removeData();
-    realtime.addData([Number(data)], label);
+    realtime.addData([Number(item.online)], item.time);
   }
 });
+
