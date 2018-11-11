f991b1bd7eff2495330281b44774085c31998b11
Grinch <anger@211.ru>
2018-11-11 20:53:23 +0700

    ok

--- websocket/counter/js/counter.js
+++ websocket/counter/js/counter.js
@@ -1,1 +1,16 @@
 'use strict';
+const counter = document.querySelector('.counter');
+const errors = document.querySelector('output.errors')
+
+const connection = new WebSocket('wss://neto-api.herokuapp.com/counter');
+
+window.addEventListener('beforeunload', () => {
+  connection.close(1000, '');
+});
+
+connection.addEventListener('message', event => {
+  const resp = JSON.parse(event.data);
+  counter.innerText = resp.connections
+  errors.value = resp.errors
+})
+
