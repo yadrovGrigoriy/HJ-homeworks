a774377d8207bde8ec1416b62113c556cf2db78d
Grinch <anger@211.ru>
2018-11-12 00:06:14 +0700

    ok

--- comet/draw/js/socket.js
+++ comet/draw/js/socket.js
@@ -1,1 +1,24 @@
+
 'use strict';
+
+const connection = new WebSocket('wss://neto-api.herokuapp.com/draw');
+
+connection.addEventListener('open', event => {
+	editor.addEventListener('update', sendImage);
+});
+
+connection.addEventListener('close', event => {
+	editor.removeEventListener('update', sendImage);
+});
+
+connection.addEventListener('error', error => {
+	console.error(error.data);
+})
+
+window.addEventListener('beforeunload', () => {
+	connection.close(1000, 'Work is done');
+});
+
+function sendImage(event) {
+	event.canvas.toBlob(blob => connection.send(blob));
+}
